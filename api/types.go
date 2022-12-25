969ed235f856f8b288146a5eeca861bff29e88c8
ycjcl868 <chaolinjin@gmail.com>
2022-12-25 11:24:08 +0800

    chore: types

---
+++ api/types.go
@@ -0,0 +1,63 @@
+package api
+
+import (
+	"time"
+)
+
+type Response struct {
+	Msg       string `json:"text"`
+	ChatID    int64  `json:"chat_id"`
+	Method    string `json:"method"`
+	ReplyToID int    `json:"reply_to_message_id"`
+}
+
+type GithubResponse struct {
+	Content struct {
+		Name        string `json:"name"`
+		Path        string `json:"path"`
+		Sha         string `json:"sha"`
+		Size        int    `json:"size"`
+		URL         string `json:"url"`
+		HTMLURL     string `json:"html_url"`
+		GitURL      string `json:"git_url"`
+		DownloadURL string `json:"download_url"`
+		Type        string `json:"type"`
+		Links       struct {
+			Self string `json:"self"`
+			Git  string `json:"git"`
+			HTML string `json:"html"`
+		} `json:"_links"`
+	} `json:"content"`
+	Commit struct {
+		Sha     string `json:"sha"`
+		NodeID  string `json:"node_id"`
+		URL     string `json:"url"`
+		HTMLURL string `json:"html_url"`
+		Author  struct {
+			Name  string    `json:"name"`
+			Email string    `json:"email"`
+			Date  time.Time `json:"date"`
+		} `json:"author"`
+		Committer struct {
+			Name  string    `json:"name"`
+			Email string    `json:"email"`
+			Date  time.Time `json:"date"`
+		} `json:"committer"`
+		Tree struct {
+			Sha string `json:"sha"`
+			URL string `json:"url"`
+		} `json:"tree"`
+		Message string `json:"message"`
+		Parents []struct {
+			Sha     string `json:"sha"`
+			URL     string `json:"url"`
+			HTMLURL string `json:"html_url"`
+		} `json:"parents"`
+		Verification struct {
+			Verified  bool        `json:"verified"`
+			Reason    string      `json:"reason"`
+			Signature interface{} `json:"signature"`
+			Payload   interface{} `json:"payload"`
+		} `json:"verification"`
+	} `json:"commit"`
+}
